09ccc9b0bc160acd8ad844065cd785a24beecaa0
zerogon <safoma@naver.com>
2024-10-29 15:19:01 +0900

    feat: delete lab file and create producer

---
+++ src/main/java/web/kafka/RestApiProducer.java
@@ -0,0 +1,10 @@
+package web.kafka;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiProducer {
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiProducer.class, args);
+    }
+}
